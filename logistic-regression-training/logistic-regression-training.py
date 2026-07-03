11a27a442bf46ba47415e9a4f21b07f8020539c7
Andrew Mos <85971409+andresmosqueraw@users.noreply.github.com>
2026-07-03 14:51:06 -0500

    Solve logistic-regression-training

--- logistic-regression-training/logistic-regression-training.py
+++ logistic-regression-training/logistic-regression-training.py
@@ -10,22 +10,16 @@ def train_logistic_regression(X, y, lr=0.1, steps=1000):
     Return (w, b).
     """
     # Write code here
-    X = np.array(X, dtype=float)
-    y = np.array(y, dtype=float)
-
     N, D = X.shape
-
     w = np.zeros(D)
     b = 0.0
 
     for _ in range(steps):
         z = X @ w + b
         p = _sigmoid(z)
-
-        dw = (X.T @ (p-y)) / N
+        dw = (X.T @ (p-y)) // N
         db = np.mean(p-y)
-
         w -= lr * dw
         b -= lr * db
 
-    return (w,b)
+    return w, b
